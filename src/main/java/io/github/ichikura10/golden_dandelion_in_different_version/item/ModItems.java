4224c7bda5dc50f08d67c1f0c814ecf078a500e9
ichikura10 <ichikura.minecraft@gmail.com>
2026-01-31 11:49:53 +0900

    tagとrecipeの追加

--- src/main/java/io/github/ichikura10/golden_dandelion_in_different_version/item/ModItems.java
+++ src/main/java/io/github/ichikura10/golden_dandelion_in_different_version/item/ModItems.java
@@ -2,8 +2,10 @@ package io.github.ichikura10.golden_dandelion_in_different_version.item;
 
 import io.github.ichikura10.golden_dandelion_in_different_version.Golden_dandelion_in_different_version;
 import io.github.ichikura10.golden_dandelion_in_different_version.block.ModBlocks;
+import net.minecraft.world.food.Foods;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SuspiciousStewItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -13,6 +15,12 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Golden_dandelion_in_different_version.MOD_ID );
 
     public static final RegistryObject<Item> GOLDEN_DANDELION = ITEMS.register("golden_dandelion",() -> new BlockItem(ModBlocks.GOLDEN_DANDELION.get(), new Item.Properties()));
+
+    public static final RegistryObject<Item> GOLDEN_DANDELION_STEW =
+            ITEMS.register("golden_dandelion_stew",
+                    () -> new SuspiciousStewItem(
+                            new Item.Properties().stacksTo(1).food(Foods.SUSPICIOUS_STEW)
+                    ));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
