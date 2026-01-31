56c1b50e7d3ffb5b10c3f00ffdd641254b1415e5
ichikura10 <ichikura.minecraft@gmail.com>
2026-01-31 11:28:30 +0900

    普通のたんぽぽのように置けるように変更

--- src/main/java/io/github/ichikura10/golden_dandelion_in_different_version/custom_flower/GoldenDandelionBlock.java
+++ src/main/java/io/github/ichikura10/golden_dandelion_in_different_version/custom_flower/GoldenDandelionBlock.java
@@ -1,18 +1,25 @@
 package io.github.ichikura10.golden_dandelion_in_different_version.custom_flower;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.BushBlock;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class GoldenDandelionBlock extends BushBlock {
 
     public GoldenDandelionBlock() {
         super(
                 BlockBehaviour.Properties.copy(Blocks.DANDELION)
-                        .noCollission()
-                        .instabreak()
-                        .sound(SoundType.GRASS)
         );
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level,
+                               BlockPos pos, CollisionContext context) {
+        return Blocks.DANDELION.getShape(state, level, pos, context);
     }
 }
